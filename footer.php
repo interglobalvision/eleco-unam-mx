adacfcd4073a2b2decfa708470567a0ca0ba95ad
mikeyrayvon <mikey@interglobal.vision>
2019-05-09 15:06:42 -0500

    use logo jpg

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
           <?php wp_nav_menu( array( 'menu' => 'footer' ) ); ?>
         </nav>
         <div class="grid-item item-s-12 item-l-auto">
-          <img src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/pleca_firma_UNAM.png" />
+          <img src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/pleca_firma_UNAM.jpg" />
         </div>
       </div>
       <div class="grid-row justify-between">
