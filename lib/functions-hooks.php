0f8576aca67bcbf067f285a367a7808503df31ff
mikeyrayvon <mikey@interglobal.vision>
2019-05-11 15:48:20 -0500

    add pll pages to gen

--- lib/functions-hooks.php
+++ lib/functions-hooks.php
@@ -5,35 +5,54 @@
 // Programatically create pages
 function create_custom_pages() {
   $custom_pages = array(
-    'inicio' => 'Inicio',
-    'programa' => 'Programa',
-    'expos' => 'Exposiciones',
-    'eleco' => 'El Eco',
-    'visita' => 'Visita',
-    'directorio' => 'Directorio',
-    'privacidad' => 'Política de Privacidad',
-    'prensa' => 'Prensa'
+    ['programa' => 'Programa',
+    'program' => 'Program'],
+    ['exposiciones' => 'Exposiciones',
+    'exhibitions' => 'Exhibitions'],
+    ['acercade' => 'El Eco',
+    'about' => 'El Eco'],
+    ['visita' => 'Visita',
+    'visit' => 'Visit'],
+    ['directorio' => 'Directorio',
+    'directory' => 'Directory'],
+    ['privacidad' => 'Política de Privacidad',
+    'privacy' => 'Privacy Policy'],
+    ['prensa' => 'Prensa',
+    'press' => 'Press'],
+    ['publicaciones' => 'Publicaciones',
+    'publicationes' => 'Publications']
   );
-  foreach($custom_pages as $page_name => $page_title) {
-    $page = get_page_by_path($page_name);
-    if( empty($page) ) {
-      wp_insert_post( array(
-        'post_type' => 'page',
-        'post_title' => $page_title,
-        'post_name' => $page_name,
-        'post_status' => 'publish'
-      ));
+  foreach($custom_pages as $page_pair) {
+    foreach($page_pair as $page_name => $page_title) {
+      $page = get_page_by_path($page_name);
+      if( empty($page) ) {
+        wp_insert_post( array(
+          'post_type' => 'page',
+          'post_title' => $page_title,
+          'post_name' => $page_name,
+          'post_status' => 'publish'
+        ));
+      }
+    }
+    if (function_exists('pll_set_post_language')) {
+      $page_es = get_page_by_path(key(array_slice($page_pair, 0, 1, true)));
+      $page_en = get_page_by_path(key(array_slice($page_pair, 1, 1, true)));
+      pll_set_post_language($page_es->ID, 'es');
+      pll_set_post_language($page_en->ID, 'en');
+      pll_save_post_translations( array(
+        'es' => $page_es->ID,
+        'en' => $page_en->ID
+      ) );
     }
   }
-  // Set static Homepage
-  $home = get_page_by_path('inicio');
-  update_option( 'page_on_front', $home->ID );
-  update_option( 'show_on_front', 'page' );
 }
 add_filter( 'after_setup_theme', 'create_custom_pages' );
 
 // WP Nav Menu
-function register_igv_nav_menu() {
-  register_nav_menu('footer',__( 'Footer Menu' ));
+function register_igv_nav_menus() {
+  register_nav_menu(array(
+    'header',__( 'Header Menu' ),
+    'footer',__( 'Footer Menu' )
+  ));
 }
-add_action( 'init', 'register_igv_nav_menu' );
+add_action( 'init', 'register_igv_nav_menus' );
