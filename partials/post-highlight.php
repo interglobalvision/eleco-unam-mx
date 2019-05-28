40b6f9a2e9832047bff2cb6959ef1e925af06807
mikeyrayvon <mikey@interglobal.vision>
2019-05-28 17:41:43 -0500

    fix post highlight widows

--- partials/post-highlight.php
+++ partials/post-highlight.php
@@ -5,7 +5,7 @@
         <div class="block-category"><span><?php echo igv_pll_cat('Entrada', 'Post', $post->ID); ?></span></div>
         <div><span><?php echo igv_post_author($post->ID); ?></span></div>
       </div>
-      <h2 class="font-serif font-size-big margin-bottom-mid"><?php the_title(); ?></h2>
+      <h2 class="font-serif font-size-big margin-bottom-mid js-fix-widows"><?php the_title(); ?></h2>
       <?php the_post_thumbnail('header-featured'); ?>
       <div class="grid-row font-size-small justify-center margin-top-basic">
         <div class="grid-item no-gutter item-s-12 item-m-11 item-l-10">
