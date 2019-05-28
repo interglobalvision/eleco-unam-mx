02478ffc5873596d9890019c209a305ff646c8c6
mikeyrayvon <mikey@interglobal.vision>
2019-05-28 17:41:35 -0500

    add margin to featured post cat

--- partials/post-featured.php
+++ partials/post-featured.php
@@ -2,7 +2,7 @@
   <article <?php post_class('grid-item item-s-12 no-gutter grid-row'); ?> id="post-<?php the_ID(); ?>">
     <div id="post-featured-image-holder" style="background-image: url(<?php echo get_the_post_thumbnail_url($post->ID, 'header-featured'); ?>)"></div>
     <a href="<?php the_permalink() ?>" class="grid-item no-gutter item-s-12 grid-row">
-      <div class="grid-item no-gutter item-s-12 item-m-8 offset-m-4 item-l-6 offset-l-6 grid-row font-size-tiny margin-bottom-small">
+      <div class="grid-item no-gutter item-s-12 item-m-8 offset-m-4 item-l-6 offset-l-6 grid-row font-size-tiny margin-bottom-small margin-top-small">
         <div class="grid-item item-s-6 item-l-4">
           <div class="block-category u-inline-block"><span><?php echo igv_pll_cat('Entrada', 'Post', $post->ID); ?></span></div>
         </div>
