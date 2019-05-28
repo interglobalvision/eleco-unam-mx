6cd4801281a6611df03bc0dd83641b3f2dd543e5
mikeyrayvon <mikey@interglobal.vision>
2019-05-27 20:00:16 -0500

    add some styling classes

--- partials/quick-info.php
+++ partials/quick-info.php
@@ -4,8 +4,8 @@ $lang = get_locale();
 ?>
 <div id="quick-info" class="grid-row border-bottom justify-between background-grey-lite font-size-small padding-top-tiny">
   <div class="grid-item item-s-12 item-m-6 item-l-auto padding-bottom-tiny">
-    <h4 class="font-uppercase font-size-tiny"><?php echo $lang === 'en_US' ? 'Address' : 'Dirección'; ?></h4>
-    <div>
+    <h4 class="font-uppercase font-size-tiny margin-bottom-half-em"><?php echo igv_pll_str('Ubicación', 'Location'); ?></h4>
+    <div class="p-no-margin-bottom">
       <?php
         if ($lang === 'en_US' && !empty($options['generalinfo_address_en'])) {
           echo apply_filters('the_content', $options['generalinfo_address_en']);
@@ -16,8 +16,8 @@ $lang = get_locale();
     </div>
   </div>
   <div class="grid-item item-s-12 item-m-6 item-l-auto padding-bottom-tiny">
-    <h4 class="font-uppercase font-size-tiny"><?php echo $lang === 'en_US' ? 'Hours' : 'Horario'; ?></h4>
-    <div>
+    <h4 class="font-uppercase font-size-tiny margin-bottom-half-em"><?php echo igv_pll_str('Horario', 'Hours'); ?></h4>
+    <div class="p-no-margin-bottom">
       <?php
         if ($lang === 'en_US' && !empty($options['generalinfo_hours_en'])) {
           echo apply_filters('the_content', $options['generalinfo_hours_en']);
@@ -28,17 +28,17 @@ $lang = get_locale();
     </div>
   </div>
   <div class="grid-item item-s-12 item-m-6 item-l-auto padding-bottom-tiny">
-    <h4 class="font-uppercase font-size-tiny"><?php echo $lang === 'en_US' ? 'Subscribe' : 'Suscribir'; ?></h4>
+    <h4 class="font-uppercase font-size-tiny margin-bottom-half-em"><?php echo igv_pll_str('Suscribir', 'Subscribe'); ?></h4>
     <form>
     </form>
   </div>
   <div class="grid-item item-s-12 item-m-6 item-l-auto padding-bottom-tiny">
-    <h4 class="font-uppercase font-size-tiny"><?php echo $lang === 'en_US' ? 'Follow' : 'Seguir'; ?></h4>
-    <ul class="u-inline-list">
+    <h4 class="font-uppercase font-size-tiny margin-bottom-half-em"><?php echo igv_pll_str('Seguir', 'Follow'); ?></h4>
+    <ul class="u-inline-list" id="social-list">
       <?php
-        echo !empty($options['socialmedia_instagram']) ? '<li><a href="https://instagram.com/' . $options['socialmedia_instagram'] . '">IG</a></li>' : '';
-        echo !empty($options['socialmedia_facebook_url']) ? '<li><a href="' . $options['socialmedia_facebook_url'] . '">FB</a></li>' : '';
-        echo !empty($options['socialmedia_twitter']) ? '<li><a href="https://twitter.com/' . $options['socialmedia_twitter'] . '">TW</a></li>' : '';
+        echo !empty($options['socialmedia_instagram']) ? '<li><a class="link-underline" href="https://instagram.com/' . $options['socialmedia_instagram'] . '">IG</a></li>' : '';
+        echo !empty($options['socialmedia_facebook_url']) ? '<li><a class="link-underline" href="' . $options['socialmedia_facebook_url'] . '">FB</a></li>' : '';
+        echo !empty($options['socialmedia_twitter']) ? '<li><a class="link-underline" href="https://twitter.com/' . $options['socialmedia_twitter'] . '">TW</a></li>' : '';
       ?>
     </ul>
   </div>
