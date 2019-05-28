50f24af97c3988ef8b7f292177051a27baa3ceae
mikeyrayvon <mikey@interglobal.vision>
2019-05-28 18:36:29 -0500

    wrap search dummy

--- partials/search.php
+++ partials/search.php
@@ -1,5 +1,5 @@
 <div class="grid-row border-bottom padding-top-micro padding-bottom-micro">
   <div class="grid-item item-s-12 text-align-center">
-    <?php echo get_locale() === 'en_US' ? 'Search' : 'Buscar'; ?>
+    <span id="search-field"><?php echo get_locale() === 'en_US' ? 'Search' : 'Buscar'; ?></span>
   </div>
 </div>
