a4ca97fd23c11b714d713411c2a9c706d4679f66
mikeyrayvon <mikey@interglobal.vision>
2019-05-30 16:06:08 -0500

    move search out of partial

--- partials/search.php
+++
@@ -1,8 +0,0 @@
-<div class="grid-row border-bottom">
-  <div class="grid-item item-s-12 text-align-center padding-top-micro padding-bottom-micro">
-    <h3><?php echo get_locale() === 'en_US' ? 'Search' : 'Buscar'; ?></h3>
-  </div>
-  <div class="grid-item item-s-12 text-align-center padding-bottom-micro">
-    <div class="search-field"></div>
-  </div>
-</div>
