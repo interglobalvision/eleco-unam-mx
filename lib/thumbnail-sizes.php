63a30d3314ca3920c5b5246325ae833886e6c399
mikeyrayvon <mikey@interglobal.vision>
2019-06-05 16:53:36 -0500

    fix image sizes for retina

--- lib/thumbnail-sizes.php
+++ lib/thumbnail-sizes.php
@@ -8,11 +8,11 @@ if( function_exists( 'add_image_size' ) ) {
   add_image_size( 'admin-thumb', 150, 150, false );
   add_image_size( 'opengraph', 1200, 630, true );
 
-  add_image_size( 'header-featured', 910, 500, false);
+  add_image_size( 'header-featured', 1820, 1000, false);
 
-  add_image_size( 'archive-thumb', 430, 230, true);
+  add_image_size( 'archive-thumb', 860, 460, true);
 
-  add_image_size( 'archive-event', 9999, 200, false);
+  add_image_size( 'archive-event', 9999, 400, false);
 
   add_image_size( 'gallery', 1200, 9999, false );
 }
