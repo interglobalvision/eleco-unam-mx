fbe37a17a61139081d737b873a43a7a1d59986b3
mikeyrayvon <mikey@interglobal.vision>
2019-07-24 13:44:48 -0500

    fix event archive pagination

--- partials/evento-archive-past.php
+++ partials/evento-archive-past.php
@@ -1,10 +1,11 @@
 <?php
 $now = time();
+$num_posts = 2;
 $args = array(
   'post_type' => 'evento',
-  'posts_per_page' => -1,
   'orderby' => 'meta_value_num',
   'order' => 'DESC',
+  'posts_per_page' => $num_posts,
   'meta_key' => '_igv_evento_datetime',
   'meta_query' => array(
     array(
@@ -18,13 +19,18 @@ $past_query = new WP_Query($args);
 
 if ($past_query->have_posts()) {
   $count = $past_query->post_count;
+?>
+<div class="grid-row js-post-holder" data-offset="<?php echo $num_posts; ?>" data-template="evento-item" data-type="evento">
+<?php
   while ($past_query->have_posts()) {
     $past_query->the_post();
 
     get_template_part('partials/evento-past-item');
   }
-
-  if ($count > 12) {
+?>
+</div>
+<?php
+  if ($count > $num_posts) {
     get_template_part('partials/see-more');
   }
 }
