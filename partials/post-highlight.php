986c0b45532732175e4f51dd8494f79d3084d996
mikeyrayvon <mikey@interglobal.vision>
2019-07-25 16:49:14 -0500

    add mobile highlight title

--- partials/post-highlight.php
+++ partials/post-highlight.php
@@ -5,8 +5,9 @@
         <span class="block-category"><?php echo igv_pll_cat($post->ID); ?></span>
         <span class="post-item-authors"><?php echo igv_post_author($post->ID); ?></span>
       </div>
-      <h2 class="font-serif font-size-big margin-bottom-mid js-fix-widows"><?php the_title(); ?></h2>
-      <?php the_post_thumbnail('header-featured'); ?>
+      <h2 class="font-serif font-size-big margin-bottom-mid js-fix-widows not-mobile"><?php the_title(); ?></h2>
+      <div><?php the_post_thumbnail('header-featured'); ?></div>
+      <h2 class="font-serif margin-top-micro font-size-item-title mobile-only"><?php the_title(); ?></h2>
       <div class="grid-row font-size-small justify-center margin-top-basic">
         <div class="grid-item no-gutter item-s-12 item-m-11 item-l-10">
           <?php the_excerpt(); ?>
