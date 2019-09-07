b3627b769a24acbb8a6fc83d1d9de295fd8f18c1
mikeyrayvon <mikey@interglobal.vision>
2019-09-07 15:41:05 +0200

    improve featured post

--- partials/post-featured.php
+++ partials/post-featured.php
@@ -1,23 +1,21 @@
-<div class="grid-row border-bottom background-grey-lite padding-top-mid padding-bottom-mid" id="post-featured-holder">
-  <article <?php post_class('grid-item item-s-12 no-gutter grid-row'); ?> id="post-<?php the_ID(); ?>">
-    <div class="grid-item no-gutter item-s-12 item-m-8 offset-m-4 item-l-6 offset-l-6 grid-row font-size-tiny margin-bottom-micro margin-top-small">
-      <div class="grid-item item-s-6 item-l-4">
-        <div class="block-category u-inline-block"><span><?php echo igv_pll_cat($post->ID); ?></span></div>
-      </div>
-      <div class="grid-item item-s-6 item-l-8">
-        <div><span><?php echo igv_post_author($post->ID); ?></span></div>
-      </div>
-    </div>
-    <div class="grid-row grid-item item-s-12 no-gutter">
-      <div id="post-featured-image-holder" style="background-image: url(<?php echo get_the_post_thumbnail_url($post->ID, 'header-featured'); ?>)"></div>
-      <a href="<?php the_permalink() ?>" class="grid-item no-gutter item-s-12 grid-row">
-        <div id="post-featured-title-holder" class="grid-item item-s-12 item-m-11 offset-m-1 item-l-10 offset-l-2 item-xl-8 offset-xl-4 margin-bottom-small">
-          <h2 class="font-serif font-size-extra js-fix-widows"><?php the_title(); ?></h2>
+<div id="post-featured-holder" class="border-bottom background-grey-lite padding-top-mid padding-bottom-mid">
+  <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+    <a href="<?php the_permalink() ?>" class="grid-row">
+      <div  id="post-featured-cat-holder" class="item-s-12 grid-row margin-bottom-micro">
+        <div class="grid-item">
+          <div class="block-category"><span><?php echo igv_pll_cat($post->ID); ?></span></div>
         </div>
-        <div class="grid-item item-m-12 item-m-4 offset-m-8">
-          <?php the_excerpt(); ?>
-        </div>
-      </a>
-    </div>
+        <div class="grid-item "><span><?php echo igv_post_author($post->ID); ?></span></div>
+      </div>
+      <div id="post-featured-image-holder" class="grid-item item-s-12 grid-column">
+        <div id="post-featured-image" class="flex-grow" style="background-image: url(<?php echo get_the_post_thumbnail_url($post->ID, 'header-featured'); ?>)"></div>
+      </div>
+      <div id="post-featured-title-holder" class="grid-item item-s-12 item-m-11 offset-m-1 item-l-10 offset-l-2 item-xl-8 offset-xl-4 margin-bottom-small padding-top-small">
+        <h2 class="font-serif font-size-extra js-fix-widows"><?php the_title(); ?></h2>
+      </div>
+      <div id="post-featured-excerpt-holder" class="grid-item item-m-12 item-m-4 offset-m-8">
+        <?php the_excerpt(); ?>
+      </div>
+    </a>
   </article>
 </div>
